78617284218bfcdb9e7b90f846648dada86a5708
Eli <116824789+Smith-Eli@users.noreply.github.com>
2024-02-23 03:13:50 +0000

    Added a refresh on the qrcode so it makes a new one every time

--- script.js
+++ script.js
@@ -8,26 +8,34 @@ const matchData = { scout: "", team: "", match: "" };
 
 function startScouting() {
 
+  let qrcodeElement = document.getElementById("qrcode");
+  
+  while (qrcodeElement.firstChild) {
+    
+    qrcodeElement.removeChild(qrcodeElement.firstChild);
+    
+  }
+    
   scout = document.getElementById("frmScout").value;
 
   let teamform = document.getElementById("frmTeams");
-
+  
   team = teamform.options[teamform.selectedIndex].value;
 
   let matchform = document.getElementById("frmMatch");
-
+  
   match = matchform.options[matchform.selectedIndex].value;
 
   matchData.scout = scout;
-
+  
   matchData.team = team;
-
+  
   matchData.match = match;
 
   let full = JSON.stringify(matchData);
 
   alert("Making QR Code: " + full);
-
+  
   new QRCode(document.getElementById("qrcode"), full);
 
 }
